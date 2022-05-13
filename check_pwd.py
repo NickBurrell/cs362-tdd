8052bd5d02ed6b968ac26b36a47efa3c4bfcf6df
Nicholas Burrell <nicholas.burrell@protonmail.com>
2022-05-12 20:38:18 -0700

    Add tests for lower case. All tests pass

--- check_pwd.py
+++ check_pwd.py
@@ -1,10 +1,14 @@
 def check_pwd(pwd):
     has_upper = False
+    has_lower = False
     if(len(pwd) < 8 or len(pwd) > 20):
         return False
     for c in pwd:
         if c.isupper():
             has_upper = True
+        if c.islower():
+            has_lower = True
 
-    return has_upper
+
+    return has_upper and has_lower
 
--- tests.py
+++ tests.py
@@ -16,6 +16,15 @@ class Main(unittest.TestCase):
     def test_contains_upper_3(self):
         self.assertFalse(check_pwd("AAAAAAA"))
 
+    def test_contains_lower_1(self):
+        self.assertTrue(check_pwd("abcdefgHi"))
+
+    def test_contains_lower_2(self):
+        self.assertTrue(check_pwd("abcdefghijklMNOP"))
+
+    def test_contains_lower_3(self):
+        self.assertFalse(check_pwd("AAAAAAA"))
+
 
 if __name__ == "__main__":
     unittest.main()
