b50ad12b1b71ba43fe031b685539ce4f17da5c37
Nicholas Burrell <nicholas.burrell@protonmail.com>
2022-05-12 20:56:58 -0700

    Add test for symbols. All tests pass.

--- check_pwd.py
+++ check_pwd.py
@@ -1,7 +1,8 @@
 def check_pwd(pwd):
-    has_upper = False
-    has_lower = False
-    has_digit = False
+    has_upper  = False
+    has_lower  = False
+    has_digit  = False
+    has_symbol = False
     if(len(pwd) < 8 or len(pwd) > 20):
         return False
     for c in pwd:
@@ -11,6 +12,9 @@ def check_pwd(pwd):
             has_lower = True
         if c.isdigit():
             has_digit = True
+        if c in "~`!@#$%^&*()_+-=":
+            has_symbol = True
 
-    return has_upper and has_lower and has_digit
+
+    return has_upper and has_lower and has_digit and has_symbol
 
--- tests.py
+++ tests.py
@@ -8,31 +8,40 @@ class Main(unittest.TestCase):
         self.assertFalse(check_pwd(""))
     
     def test_contains_upper_1(self):
-        self.assertTrue(check_pwd("1abcdefgHi"))
+        self.assertTrue(check_pwd("1`abcdefgHi"))
 
     def test_contains_upper_2(self):
-        self.assertTrue(check_pwd("1abcdefghijklMNOP"))
+        self.assertTrue(check_pwd("1abcdefg=hijklMNOP"))
 
     def test_contains_upper_3(self):
-        self.assertFalse(check_pwd("AAAAAAA"))
+        self.assertFalse(check_pwd("AAAAAA+A"))
 
     def test_contains_lower_1(self):
-        self.assertTrue(check_pwd("1abcdefgHi"))
+        self.assertTrue(check_pwd("1abcdefgH_i"))
 
     def test_contains_lower_2(self):
-        self.assertTrue(check_pwd("1abcdefghijklMNOP"))
+        self.assertTrue(check_pwd("1a%bcdefgh1ijklMNOP"))
 
     def test_contains_lower_3(self):
-        self.assertFalse(check_pwd("1AAAAAAA"))
+        self.assertFalse(check_pwd("1AAAAAA-A"))
 
     def test_contains_digit_1(self):
-        self.assertTrue(check_pwd("abcdefgH2"))
+        self.assertTrue(check_pwd("abcdefg@H2"))
 
     def test_contains_digit_2(self):
-        self.assertTrue(check_pwd("abcdefghijklMNOP1"))
+        self.assertTrue(check_pwd("abcdefgh&ijklMNOP1"))
 
     def test_contains_digit_3(self):
         self.assertFalse(check_pwd("AAAAAAA"))
+
+    def test_contains_symbol_1(self):
+        self.assertTrue(check_pwd("abcdefg^H2"))
+
+    def test_contains_symbol_2(self):
+        self.assertTrue(check_pwd("bghi()jklMNOP1"))
+
+    def test_contains_symbol_3(self):
+        self.assertFalse(check_pwd("AAAAAAA-"))
 
 
 
