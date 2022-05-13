fdfa28ea45436be9737f1e5fb67224915f2a3939
Nicholas Burrell <nicholas.burrell@protonmail.com>
2022-05-12 20:33:12 -0700

    Wrote three tests to check if string is length between 8 and 20, and has an uppercase character

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,10 @@
 def check_pwd(pwd):
+    has_upper = False
     if(len(pwd) < 8 or len(pwd) > 20):
         return False
+    for c in pwd:
+        if c.isupper():
+            has_upper = True
 
+    return has_upper
 
--- tests.py
+++ tests.py
@@ -3,8 +3,19 @@ import unittest
 from check_pwd import check_pwd
 
 class Main(unittest.TestCase):
+        
     def test_nonempty(self):
         self.assertFalse(check_pwd(""))
+    
+    def test_contains_upper_1(self):
+        self.assertTrue(check_pwd("abcdefgHi"))
+
+    def test_contains_upper_2(self):
+        self.assertTrue(check_pwd("abcdefghijklMNOP"))
+
+    def test_contains_upper_3(self):
+        self.assertFalse(check_pwd("AAAAAAA"))
+
 
 if __name__ == "__main__":
     unittest.main()
